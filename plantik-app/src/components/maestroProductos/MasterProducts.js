8f622cabd2173987a4121cfe07e439c74f57be89
Brian Villada <brian.villada@udea.edu.co>
2021-10-01 17:02:37 -0500

    Se termina de cambiar tamaños en archivo MasterProducst.js

--- plantik-app/src/components/maestroProductos/MasterProducts.js
+++ plantik-app/src/components/maestroProductos/MasterProducts.js
@@ -1,15 +1,42 @@
 import React from 'react'
 import './MasterProducts.css'
-import { Form, Button, Container, Row, Col, FormText } from 'react-bootstrap'
+import { Form, Button, Container, Row, Col, Card } from 'react-bootstrap'
 
 export const MasterProducts = () => {
   return (
     <>
       <Container className="product-container">
         <Row className="justify-content-md-center">
-          <Col xs={6} className="regVentas-form">
+          <Col xs={7} className="form-MasterProducts">
             <h2> Maestro de Productos </h2>{' '}
-            <form>
+            <Form>
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="labelBuscar">
+                  <Form.Label>Buscar un producto: </Form.Label>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Control type="text" placeholder="Ingrese el Código" />
+                </Form.Group>
+                <Form.Group as={Col} controlId="buttonMostrar">
+                  <Button variant="primary" type="submit" disabled>
+                    Buscar
+                  </Button>
+                </Form.Group>
+                <Row className="mb-3" xs={12}>
+                  <Form.Group as={Col} controlId="CardProductos">
+                    <Card>
+                      <Card.Header>Productos</Card.Header>
+                      <Card.Body>
+                        <Card.Title>Ningun Producto Encontrado</Card.Title>
+                        <Card.Text>Descripcion del producto</Card.Text>
+                        <Button variant="primary" disabled>
+                          Actualizar
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  </Form.Group>
+                </Row>
+              </Row>
               <Row className="mb-3">
                 <Form.Group as={Col} controlId="labelMostrar">
                   <Form.Label>Ver lista de Productos: </Form.Label>
@@ -23,7 +50,7 @@ export const MasterProducts = () => {
                     <option value="5">Insecticidas</option>
                     <option value="6">Accesorios y Otros</option>
                   </Form.Select>
-                  <Form.Text className="text-muted">
+                  <Form.Text className="text-Descriptive">
                     Seleccione una categoria
                   </Form.Text>
                 </Form.Group>
@@ -33,20 +60,7 @@ export const MasterProducts = () => {
                   </Button>
                 </Form.Group>
               </Row>
-              <Row className="mb-3">
-                <Form.Group as={Col} controlId="labelBuscar">
-                  <Form.Label>Buscar un producto: </Form.Label>
-                </Form.Group>
-                <Form.Group as={Col}>
-                  <Form.Control type="text" placeholder="Ingrese el Código" />
-                </Form.Group>
-                <Form.Group as={Col} controlId="buttonMostrar">
-                  <Button variant="primary" type="submit" disabled>
-                    Buscar
-                  </Button>
-                </Form.Group>
-              </Row>
-            </form>
+            </Form>
           </Col>
         </Row>
       </Container>
