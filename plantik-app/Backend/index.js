01651f81a5ea172b443b6a85ee2753d89bfe3718
juansalas22 <j.charle.salas@gmail.com>
2021-10-16 16:18:15 -0500

    update

---
+++ plantik-app/Backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+require('dotenv').config();
+const { dbConnection } = require('./db/config');
+const cors = require('cors');
+
+
+
+//Crear el servidor de express
+const app = express();
+
+//Base de datos
+dbConnection();
+
+// CORS
+app.use(cors())
+
+// Directorio Público
+app.use( express.static('public') );
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+// Rutas
+app.use('/api/auth', require('./routes/auth') );
+
+//escuchar peticiones
+app.listen(  process.env.PORT , () => {
+    console.log(`Servidor corriedo en puerto ${ process.env.PORT }`);
+});
